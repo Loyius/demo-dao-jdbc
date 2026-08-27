eb6fd6506e94df79a1d9ec5410b69fe35c6af465
Larissa <larissaqramos1@gmail.com>
2026-08-27 16:32:29 -0300

    feat: Added "Department" entity

---
+++ READ.md
@@ -0,0 +1,11 @@
+# Project Architecture
+
+### Department entity class
+Entity class check list:
+    . Attributes
+    . Constructors
+    . Getters/Setters
+    . hashCode and equals
+    . toString
+    . implements Serializable
+
--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
 import db.DB;
+import model.entities.Department;
 
 import java.sql.Connection;
 
 public class Program {
     public static void main(String[] args) {
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        //Connection conn = DB.getConnection();
+       //DB.closeConnection();
+
+        Department department = new Department(1, "Books");
+
+        System.out.println(department.toString());
     }
 }
